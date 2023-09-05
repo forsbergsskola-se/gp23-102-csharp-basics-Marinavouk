f89ae6aa214bb91e533de54df32b3e1cf0585e23
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-09-05 15:21:31 +0200

    P7Input

---
+++ P7Input/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("What's your name?");
+string answer = Console.ReadLine();
+
+Console.WriteLine($"Hello, {answer}");
