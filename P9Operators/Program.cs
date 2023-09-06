2938cc9733342fcfb31c9e289b79953d66965556
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-09-06 13:28:34 +0200

    exercises1

---
+++ P9Operators/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Give me a number of seconds");
+string number = Console.ReadLine();
+float input = float.Parse(number);
+float tim = input * 60 * 60;
+float d = tim / 24;
+float h = tim % 24;
+float m = h % 60;
+float s = m % 60;
+  
+
+Console.WriteLine($"Seconds: {s}");
+Console.WriteLine($"Minutes: {m}");
+Console.WriteLine($"Hours: {h}");
+Console.WriteLine($"Days: {d}");
+Console.WriteLine($"{d}. {h} : {m} : {s} ");
---
+++ exercises/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Give me a number");
+
+string input = Console.ReadLine();
+int i = int.Parse(input);
+int m = i * 60 ;
+
+Console.WriteLine($"{m}");
