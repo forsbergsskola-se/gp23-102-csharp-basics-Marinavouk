75966c0860c8d48ebf3c3ba74a71ee3eb2aa6b8e
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-09-06 14:17:50 +0200

    Exercise4

---
+++ Exercise4/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Develop a program that calculates the area of a 
+//circle using a float input for the radius and displays the result
+
+Console.WriteLine("Give me the radious number ");
+string radios = Console.ReadLine();
+float r = float.Parse(radios);
+double pi = 3.14159265359;
+
+double a = pi * r*r ;
+
+Console.WriteLine($"{a}");
+
+
