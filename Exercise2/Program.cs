a6d0180d6c6c686834064237658a661efbf79af5
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-09-06 13:55:29 +0200

    Exercise2Remade

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -7,7 +7,7 @@ int one = int.Parse(input1);
 string input2 = Console.ReadLine();
 int two = int.Parse(input2);
 
-int sum = one % two;
-float fractionalNumber = sum;
+int sum = one / two;
+
 
 Console.WriteLine($"{sum}");
