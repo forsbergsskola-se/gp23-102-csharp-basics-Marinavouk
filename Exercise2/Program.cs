18fdea7e7da2b17a052715f43f7080e969e6f4c1
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-09-06 13:53:22 +0200

    Exercise2

---
+++ Exercise2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Give me 2 numbers");
+string input1 = Console.ReadLine();
+int one = int.Parse(input1);
+
+string input2 = Console.ReadLine();
+int two = int.Parse(input2);
+
+int sum = one % two;
+float fractionalNumber = sum;
+
+Console.WriteLine($"{sum}");
