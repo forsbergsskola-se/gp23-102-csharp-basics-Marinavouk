9232cb764c7c81f8f5cc85ab8bb387a63682b1ed
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-09-06 16:23:26 +0200

    Exercise9

---
+++ Exercise9/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Write a program that takes an integer input representing seconds and
+//converts it to minutes and remaining seconds, then displays the result.
+
+Console.Write("Type seconds");
+ string sec = Console.ReadLine();
+int s = int.Parse(sec);
+int r = s / 60 ;
+float rest = r % 60;
+
+
+Console.WriteLine($"Minutes: {r}");
+
+
+
