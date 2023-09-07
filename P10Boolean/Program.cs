bed97620d84aecee2b732fe87c417bd986fd0d15
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-09-07 13:25:21 +0200

    P10BooleanDone

---
+++ P10Boolean/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.Versioning;
+
+Console.WriteLine("What's your age?");
+string ageInput = Console.ReadLine();
+int age = int.Parse(ageInput);
+bool isChild = true;
+bool isTeenager = true;
+bool isGrownup = true;
+
+isChild = age <= 12;
+isTeenager = age < 19;
+isGrownup = age >= 19;
+
+Console.WriteLine($"You are a child: {isChild}");
+Console.WriteLine($"You are a teenager: {isTeenager}");
+Console.WriteLine($"You are a grown-up: {isGrownup}");
+
+
