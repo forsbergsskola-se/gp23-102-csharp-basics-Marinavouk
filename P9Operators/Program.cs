21e7ad8bce1762fb670cf24d4e2119219f48c70f
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-09-07 13:03:56 +0200

    p9operatorsdone

--- P9Operators/Program.cs
+++ P9Operators/Program.cs
@@ -3,15 +3,16 @@
 Console.WriteLine("Give me a number of seconds");
 string number = Console.ReadLine();
 float input = float.Parse(number);
-float tim = input * 60 / 24 ;
-float d = tim / 24;
-float h = tim % 24;
-float m = h % 60;
-float s = m % 60;
-  
+float day = input / (60 * 60 * 24) ;
+
+int s = (int)(input % 60);
+int m = (int)((input / 60) % 60);
+int tim = (int)((input / (60 * 60)) % 24);
+int d = (int)(day);
 
 Console.WriteLine($"Seconds: {s}");
 Console.WriteLine($"Minutes: {m}");
 Console.WriteLine($"Hours: {tim}");
 Console.WriteLine($"Days: {d}");
-Console.WriteLine($"{d}. {h} : {m} : {s} ");
+Console.WriteLine($"{d}. {tim} : {m} : {s} ");
+Console.WriteLine($"In total, that's {day}");
