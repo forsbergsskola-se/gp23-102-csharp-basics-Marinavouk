4de36608502b9fab14f3a2db47fb89966527cccc
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-09-11 21:55:12 +0200

    P11CalculatorDone

---
+++ P11Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int input1 = int.Parse(Console.ReadLine());
+string chr = Console.ReadLine();
+int input2 = int.Parse(Console.ReadLine());
+
+if (chr  =="-")
+{
+    Console.WriteLine(input1 - input2);
+}
+else if (chr == "+")
+{
+    Console.WriteLine(input1 + input2);
+}
+else if (chr == "*")
+{
+    Console.WriteLine(input1 * input2);
+}
+
+else
+{
+    Console.WriteLine(input1 / input2);
+}
+
