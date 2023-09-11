7aedcd432ee415b19b9521f43cfa8e025bea670d
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-09-11 15:31:47 +0200

    P11mINIMAXDone

---
+++ P11MiniMax/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+int input1 = int.Parse(Console.ReadLine());
+int input2 = int.Parse(Console.ReadLine());
+int input3 = int.Parse(Console.ReadLine());
+
+if (input1 < input2 && input1 < input3)
+{
+    Console.WriteLine("The minimum is: " + input1);
+}
+else if (input2 < input1 && input2 < input3)
+{
+    Console.WriteLine("The minimum is: " + input2);
+}
+else  if (input3 < input1 && input3 < input2)
+{
+    Console.WriteLine("The minimum is: " + input3);
+}
+
+if (input1 > input2 && input1 > input3)
+{
+    Console.WriteLine("The maximum is: " + input1);
+}
+
+else if (input2 > input1 && input2 > input3)
+{
+    Console.WriteLine("The maximum is: " + input2);
+}
+
+else if(input3 > input1 && input3 > input2)
+{
+     Console.WriteLine("The maximum is: " + input3);
+}
+
+
+
