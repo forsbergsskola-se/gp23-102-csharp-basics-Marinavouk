460c74e483c107bb734c344a8ccc99067f0bad55
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-09-11 22:03:36 +0200

    P11EvenOrOddDone

---
+++ p11EvenOrOdd/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design;
+
+int input1 = int.Parse(Console.ReadLine());
+
+if (input1 % 2 == 0)
+{
+Console.WriteLine("That's even!");
+}
+
+else
+{
+Console.WriteLine("That's odd!");
+}
