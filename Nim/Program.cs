98fa7f588014d3c88823cb16f40a314a409ae865
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-09-13 14:57:20 +0200

    nimNotGood

---
+++ Nim/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.ComponentModel.Design;
+
+Console.WriteLine("Welcome to Nim!");
+Console.WriteLine("|||||||||||||||||||||||| (24)");
+int totalMatches = 24;
+
+matches:
+Console.WriteLine("How many matches do you want to draw?");
+int userInput = int.Parse(Console.ReadLine());
+
+if (totalMatches >=0 && totalMatches <= 24)
+{
+    
+    int matchesLeft = totalMatches - userInput;
+    Console.WriteLine("Draw matches here later: " + matchesLeft);
+    totalMatches -= userInput;
+   
+    int aiChoice = 1;
+    int matchesChoice = totalMatches - aiChoice;
+    Console.WriteLine(matchesChoice);
+    totalMatches -= aiChoice;
+    
+    
+        goto matches;
+        
+}
+
+else
+{
+    Console.WriteLine("Game Over");
+}
