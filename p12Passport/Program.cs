5475e22e6641e5090e5fe879b76f6319135831a2
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-09-14 15:59:36 +0200

    p12RandomPassportTyp

---
+++ p12Passport/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+ var one = (byte)Random.Shared.Next();
+ var two = (byte)Random.Shared.Next();
+ var three = (byte)Random.Shared.Next();
+ var four = (byte)Random.Shared.Next();
+ var five = (byte)Random.Shared.Next();
+ var six = (byte)Random.Shared.Next();
+ var  x = System.Text.Encoding.ASCII.GetChars(new byte[]{one, two, three, four, five, six});
+ Console.WriteLine();
