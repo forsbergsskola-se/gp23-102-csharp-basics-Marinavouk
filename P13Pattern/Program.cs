b42d5067175d98ba1e76d6a790284db1f52f00fe
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-09-14 14:51:54 +0200

    p13patternDone

---
+++ P13Pattern/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int userInput = int.Parse(Console.ReadLine());
+string pattern = "#-#-#-#-#-";
+int count = 0;
+patternp13:
+if (count < userInput)
+{
+    count += 1;
+    Console.WriteLine(pattern);
+    goto patternp13;
+}
